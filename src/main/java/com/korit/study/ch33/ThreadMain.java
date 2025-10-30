93771301dab26c39f70d1ad2958d9ea31a23938c
BNDAD <ral7057@naver.com>
2025-10-30 17:45:48 +0900

    feat: 10/30 Thread

--- src/main/java/com/korit/study/ch33/ThreadMain.java
+++ src/main/java/com/korit/study/ch33/ThreadMain.java
@@ -1,21 +1,44 @@
 package com.korit.study.ch33;
 
+class Counter {
+    private int counter = 0;
+    public void increment() {
+        counter++;
+    }
+    public void decrement() {
+        counter--;
+    }
+    public int getCounter() {
+        return counter;
+    }
+}
+
+
+
+
 public class ThreadMain {
     public static void main(String[] args) {
 
+        /**
+         * Thread(쓰레드)
+         * 1. 싱글쓰레드: 쓰레드 하나만 사용의 의미(main메서드 하나만 사용)
+         * 2. 멀티쓰레드: 쓰레드 두개 이상 사용
+         */
+
+        Counter counter = new Counter();
 
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 1000; i++) {
                 System.out.println("1번 반복문: " + i);
-                sum += i;
+                counter.increment();
             }
         });
 
 
         Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                System.out.println("1번 반복문: " + i);
-                sum += i;
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("2번 반복문: " + i);
+                counter.increment();
             }
         });
 
@@ -25,8 +48,17 @@ public class ThreadMain {
 
         thread1.start();
         thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
 
-        System.out.println("쓰레드 종료");
+            System.out.println(counter.getCounter());
+            System.out.println("쓰레드 종료");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
 
     }
 }
